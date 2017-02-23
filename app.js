3af84f6850843f49b9a95da89f1d4fe889866ddf
MEGAN ARNELL <arnell.megan@yahoo.com>
2017-02-23 15:59:02 -0700

    working

---
+++ app.js
@@ -0,0 +1,77 @@
+var app = angular.module('News', ['ui.router']);
+    app.controller('MainCtrl', [
+    '$scope',
+    'postFactory',
+    function($scope, postFactory){
+      $scope.posts = postFactory.posts;
+      
+     $scope.addPost = function(){
+      if($scope.formContent === '') { return; }
+      $scope.posts.push({
+        title: $scope.formContent,
+        upvotes: 0,
+        comments: []
+      });
+      $scope.title = '';
+    };
+      
+      $scope.incrementUpvotes = function(post) {
+    post.upvotes += 1;
+  };
+    }
+    ]);
+
+app.controller('PostCtrl', [
+    '$scope',
+    '$stateParams',
+    'postFactory', 
+    function($scope, $stateParams, postFactory){
+      debugger;
+      $scope.post = postFactory.posts[$stateParams.id];
+      $scope.addComment = function(){
+        if($scope.body === '') { return; }
+        if($scope.post.comments){
+        $scope.post.comments.push({
+          body: $scope.body,
+          upvotes: 0
+        });
+        }
+        else {
+          $scope.post.comments = [{
+          body: $scope.body,
+          upvotes: 0
+        }];
+        }
+        $scope.body = '';
+      };
+    $scope.incrementUpvotes = function(comment){
+      comment.upvotes += 1; 
+    };
+  }]);
+
+  app.factory('postFactory', [function(){
+    var o = {
+      posts: []
+    };
+    return o;
+  }]);
+ 
+ app.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+      $stateProvider
+        .state('home', {
+          url: '/home',
+          templateUrl: '/home.html',
+          controller: 'MainCtrl'
+        });
+      $stateProvider
+        .state('posts', {
+          url: '/posts/{id}',
+          templateUrl: '/posts.html',
+          controller: 'PostCtrl'
+      });
+      
+      $urlRouterProvider.otherwise('/home');
+    }]);
